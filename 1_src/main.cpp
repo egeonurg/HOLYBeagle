2412c743a81d1da55f2a47439ac552d8e9ed8524
Baltazar <egeonurg@gmail.com>
2025-03-03 00:09:35 +0100

    diff_ip_config

--- 1_src/main.cpp
+++ 1_src/main.cpp
@@ -11,7 +11,11 @@ void sendLoop()
     memset(arr,0xEE,sizeof(arr));
     while (true) 
     {
-        sender.sendPacket("192.168.1.100", "192.168.1.1", arr);
+#ifdef CROSS_COMPILE_BBBI
+sender.sendPacket("192.168.8.94", "192.168.0.100", arr);
+#else
+sender.sendPacket("192.168.0.100", "192.168.8.94", arr);
+#endif
         std::this_thread::sleep_for(std::chrono::milliseconds(100));  // Wait for 1 second
     }
 }
